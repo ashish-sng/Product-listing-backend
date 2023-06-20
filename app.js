91f2af6ac1d2cdcaf6a18905ce0dee8f577a404d
Ashish Singh <ashishsingh.1899@gmail.com>
2023-06-20 15:43:29 +0530

    tweaks and refactored code

--- app.js
+++ app.js
@@ -1,6 +1,22 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const productRouter = require('./routes/productRoute');
+const commentRouter = require('./routes/commentsRoute');
+const authRouter = require('./routes/authRoute');
+
+const app = express();
+dotenv.config();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+
+app.use("/", authRouter);
+app.use("/products", productRouter);
+app.use("/comments", commentRouter);
+
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,5 +1,19 @@
+const mongoose = require("mongoose");
 const app = require("./app");
 
 const port = process.env.PORT || 4000;
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+  mongoose
+    .connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      console.log("Connected to database!");
+      console.log(`Server running on port ${port}`);
+    })
+    .catch(() => {
+      console.log("Connection failed!");
+    });
+});
