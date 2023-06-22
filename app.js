add32d75d4b64657a0046539b57ac78f9e8a4fdc
Ashish Singh <ashishsingh.1899@gmail.com>
2023-06-22 11:12:23 +0530

    improvements

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
-const productRouter = require('./routes/productRoute');
-const commentRouter = require('./routes/commentsRoute');
-const authRouter = require('./routes/authRoute');
+const productRouter = require("./routes/productRoute");
+const commentRouter = require("./routes/commentsRoute");
+const authRouter = require("./routes/authRoute");
 
 const app = express();
 dotenv.config();
@@ -14,9 +15,32 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+// Health check endpoint
+app.get("/health", (req, res) => {
+  const dbStatus =
+    mongoose.connection.readyState === 1 ? "Connected" : "Disconnected";
+
+  res.status(200).json({
+    server: "Running",
+    database: dbStatus,
+  });
+});
+
 app.use("/", authRouter);
 app.use("/products", productRouter);
 app.use("/comments", commentRouter);
+app.use((req, res, next) => {
+  const error = new Error("Not found");
+  error.status = 404;
+  next(error);
+});
 
+// Error handler middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res
+    .status(500)
+    .json({ error: "Something went wrong! Please try again later." });
+});
 
 module.exports = app;
